d79f93fc9cf8bddcd318ebf3a26b2a5dec0d25cb
Jamall Malik <mimsjamall79@gmail.com>
2022-08-08 21:30:38 -0400

    finished lab

--- api/country-finder.py
+++ api/country-finder.py
@@ -2,7 +2,6 @@ from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
 
-
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -10,16 +9,16 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         query_dict = dict(query_string_list)
 
-        if "name" in query_dict:
-            url = "https://restcountries.com/v3.1/name/"
-            query = query_dict["name"]
+        if "capital" in query_dict:
+            url = "https://restcountries.com/v3.1/capital/"
+            query = query_dict["capital"]
 
-            response = requests.get(url + query_dict["name"])
+            response = requests.get(url + query)
             data = response.json()
 
-            parsed_capital = data[0]["capital"]
-            country = str(parsed_capital[0])
-            message = f"{query} is the capital of {country}"
+            country_name = data[0]["name"]
+            country = str(country_name["common"])
+            message = f"The capital of {country} is {query}"
 
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
